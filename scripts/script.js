0d99a194335694b9f784782b8dd98c34f2e2e5f4
yelowtail <xavi.debegnach@gmail.com>
2022-06-29 23:00:57 -0300

    commit de control

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,27 @@
-const el = document.querySelector(".navbar")
-const observer = new IntersectionObserver( 
-  ([e]) => e.target.classList.toggle("is-pinned", e.intersectionRatio < 1),
-  { threshold: [1] }
-);
 
-observer.observe(el);
+// var observer = new IntersectionObserver(function(entries) {
+// 	// no intersection with screen
+// 	if(entries[0].intersectionRatio === 0)
+// 		document.querySelector(".navbar").classList.add("sticky");
+// 	// fully intersects with screen
+// 	else if(entries[0].intersectionRatio === 1)
+// 		document.querySelector(".navbar").classList.remove("sticky");
+// }, { threshold: [0,1] });
+
+// observer.observe(document.querySelector(".bottom-navbar"));
+
+
+let navbar = document.querySelector('.navbar');
+let header = document.querySelector('.header');
+
+window.addEventListener('scroll', () => {
+    let navHeight = navbar.getBoundingClientRect().height;
+    let headerHeight = header.getBoundingClientRect().height;
+    let scrolly = window.scrollY;
+    
+    if(scrolly > headerHeight-navHeight){
+        navbar.classList.add('is-pinned')
+    } else{
+        navbar.classList.remove('is-pinned')
+    }
+}, {passive:true})
